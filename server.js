1904a1f9baa481a440bf7a5b6ca397e041e453d3
Robert Ayers <rsheldon3ayers@gmail.com>
2016-02-18 10:54:16 -0600

    new note database

--- server.js
+++ server.js
@@ -9,6 +9,13 @@ const express = require('express');
 
 const app = express();
 const port = process.env.PORT || 3000;
+const Note = mongoose.model('Notes', mongoose.Schema({
+
+  title: String,
+  text: String
+}));
+
+
 
 app.set('view engine', 'jade');
 app.use(bodyParser.urlencoded({
@@ -24,10 +31,17 @@ app.get('/notes/new', (req, res) => {
   res.render('new-note');
 });
 app.post('/notes', (req, res) => {
-  console.log(req.body);
-  res.redirect('/');
+  Note.create(req.body, (err, note) => {
+    if (err) throw err;
+    console.log(note);
+    res.redirect('/');
+  })
 });
-app.listen(3000, () => {
-  console.log(`Evernode server runing on port: ${port}`)
 
+mongoose.connect('mongodb://localhost:27017/evernode', (err) => {
+  if (err) throw err;
+  app.listen(3000, () => {
+    console.log(`Evernode server runing on port: ${port}`)
+  });
 });
+
