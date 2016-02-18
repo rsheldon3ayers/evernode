107020b28b4d48bcc0038d7b8e4cdf4967f108a4
Robert Ayers <rsheldon3ayers@gmail.com>
2016-02-18 13:13:17 -0600

    added delete route

--- server.js
+++ server.js
@@ -1,47 +1,30 @@
-//express
-//mongoose
-//body-parser
 'use strict';
 
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
 const express = require('express');
+const mongoose = require('mongoose');
+
+const note = require('./routes/note');
 
 const app = express();
 const port = process.env.PORT || 3000;
-const Note = mongoose.model('Notes', mongoose.Schema({
-
-  title: String,
-  text: String
-}));
-
-
 
 app.set('view engine', 'jade');
+
 app.use(bodyParser.urlencoded({
   extended: false
 }));
 
-app.get('/', (req, res) =>{
-
+app.get('/', (req, res) => {
   res.send('Server Running');
 });
 
-app.get('/notes/new', (req, res) => {
-  res.render('new-note');
-});
-app.post('/notes', (req, res) => {
-  Note.create(req.body, (err, note) => {
-    if (err) throw err;
-    console.log(note);
-    res.redirect('/');
-  })
-});
+app.use(note);
 
 mongoose.connect('mongodb://localhost:27017/evernode', (err) => {
   if (err) throw err;
-  app.listen(3000, () => {
-    console.log(`Evernode server runing on port: ${port}`)
+
+  app.listen(port, () => {
+    console.log(`Evernode server running on port: ${port}`);
   });
 });
-
