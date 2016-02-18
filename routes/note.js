55e6a4b907ed519df631a664ac9b18e612120f37
Robert Ayers <rsheldon3ayers@gmail.com>
2016-02-18 14:51:40 -0600

    added edit and update

--- routes/note.js
+++ routes/note.js
@@ -8,8 +8,11 @@ const note = require('../controllers/note');
 router.get('/notes', note.index);
 router.get('/notes/new', note.newNote);
 router.get('/notes/:id', note.show);
+router.get('/notes/:id/edit', note.edit);
+router.put('/notes/:id', note.update);
 router.delete('/notes/:id', note.destroy);
 router.post('/notes', note.create);
+router.put('/notes/:id', note.update);
 
 module.exports = router;
 
